0b6df7239cbb4d16baf467df08d1af7368035f66
José Adauto <joseadauto923@gmail.com>
2022-06-11 12:24:35 -0300

    Header is logged

--- src/components/Header/Index.jsx
+++ src/components/Header/Index.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import './style.css';
 export default function Header() {
+   
     return (
         <Navbar bg="light" variant="light">
             <Container className='nav-main'>
@@ -9,7 +10,8 @@ export default function Header() {
                 <Nav className="flex-row">
                     <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="#features">Anúncios</Nav.Link>
-                    <Nav.Link href="/login">Entrar</Nav.Link>
+                    {sessionStorage.getItem("token") ? <Nav.Link href="/login">Minha conta</Nav.Link> : <Nav.Link href="/login">Entrar</Nav.Link> }
+                    
                     <div className='anunciar-st'>
                         <Nav.Link href="/announces" className='ann'>Anunciar</Nav.Link>
                     </div>
